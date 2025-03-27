d79011bf7f14dcb7ae7f0831218b9d6d7fda5030
grenoli-dk <92790463+grenoli-dk@users.noreply.github.com>
2025-03-27 10:48:41 +0100

    Create unbounce.js

---
+++ unbounce.js
@@ -0,0 +1,14 @@
+// shared-data.js
+
+
+var sharedData = {
+
+    "medlemstal": "+290.000",
+
+    "forsikrings_medlemstal": "+100.000",
+
+    "events_aarligt": "108.000",
+
+    "dbf_image": "https://d9hhrg4mnvzow.cloudfront.net/fordele.ida.dk/netvaerk/030697c8-dks-bedst-2024-vertical-dk-white_10xe0ae0xc0ae000000028.png"
+
+};
