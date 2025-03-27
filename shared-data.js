45ad736b9f08f4d1221b683ff479f794dd457cef
grenoli-dk <92790463+grenoli-dk@users.noreply.github.com>
2025-03-27 10:43:38 +0100

    Update shared-data.js

--- shared-data.js
+++ shared-data.js
@@ -1,7 +1,13 @@
 // shared-data.js
 
 var sharedData = {
-    "medlemstal": "+170.000",
-    "forsikrings_medlemstal": "+100.000",
-    "events_aarligt": "108.000"
+
+    "medlemstal": "+290.000",
+
+    "forsikrings_medlemstal": "+100.000",
+
+    "events_aarligt": "108.000",
+
+    "dbf_image": "https://d9hhrg4mnvzow.cloudfront.net/fordele.ida.dk/netvaerk/030697c8-dks-bedst-2024-vertical-dk-white_10xe0ae0xc0ae000000028.png"
+
 };
